94d6c91675f93d1fde68425a2f05b7e91b20236e
skypotato <hunsooski@naver.com>
2018-12-31 10:33:25 +0900

    2018.12.31_skypotato : #1 commit

--- src/main/webapp/js/comm.js
+++ src/main/webapp/js/comm.js
@@ -4,15 +4,126 @@
  * 작성일 : 2018.12.29
  */
 
-// 3.카테고리 영역
-// 4.총 개수표시 영역
-// 5.상품 리스트
-function Category(){
-	// 전체리스트, 전시 등 메뉴 항목
-	var _this = null;
-	var onload = function(element){
-		_this = element;
+/**
+ * ajax 통신 함수
+ * @param url
+ * @param callbackFn
+ * @param isAsync
+ */
+function ajax(url, callbackFn, isAsync) {
+	if (!url) {
+		return;
 	}
-	var event = function(){
+	if (typeof callbackFn != 'function') {
+		return;
+	}
+	if(!isAsync) isAsync=true; // 기본적으로 비동기 방식을 사용한다.
+	
+	var oReq = new XMLHttpRequest();
+
+	oReq.addEventListener("load", callbackFn);
+	oReq.open("GET", url, isAsync);
+	oReq.send();
+}
+/**
+ * replaceAll
+ * @param {String} str1 : 치환될 문자
+ * @param {String} str2 : 치환할 문자
+ * @returns {String} temp_str
+ * */
+String.prototype.replaceAll = function( str1, str2 ){
+	var temp_str = this;
+	if(temp_str == null || temp_str == "undefined" || temp_str == ""){
+		return "";
+	}else{
+		 temp_str = temp_str.replace(/(^\s*)|(\s*$)/gi, "");
+		 temp_str = temp_str.replace(eval("/" + str1 + "/gi"), str2);
+		 return temp_str;		
+	}
+};
+/**
+ * 카테고리 클래스
+ */
+function CategoryList(){
+	var _this = null;
+	var _ctgList = null;
+	var _resultList = null;
+	
+	this.onload = function(element, object){
+		_this = this;
+		_ctgList = element;
+		_resultList = object;
+		_this.getData();
+	}
+	/**
+	 * 데이터 조회
+	 */
+	this.getData = function(){
+		// apiURL 설정
+		var url = "api/categories";
+		
+		ajax(url, function() {
+			var jsonObj = JSON.parse(this.responseText);
+			var categories = jsonObj.items;
+			
+			_this.initCtgList();
+			
+			categories.forEach(function(item) {
+				_this.addCategory(item);
+			});
+			
+			_this.setEvent();
+			
+		}, false);
+	}
+	/**
+	 * 카테고리 리스트 초기화
+	 */
+	this.initCtgList = function(){
+		var ctgTemplate = document.querySelector("#categoryItem").innerHTML;
+		_ctgList.innerHTML = ctgTemplate.replace("${id}","0")
+										.replace("${name}","전체리스트")
+										.replace("${active}","active");
+	}
+	/**
+	 * 카테고리 추가
+	 */
+	this.addCategory = function(data){
+		var ctgTemplate = document.querySelector("#categoryItem").innerHTML;
+		ctgTemplate = ctgTemplate.replace("${id}",data.id)
+									.replace("${name}",data.name)
+									.replace("${active}","");
+		_ctgList.insertAdjacentHTML("beforeend", ctgTemplate);
+	}
+	/**
+	 * 이벤트 등록
+	 */
+	this.setEvent = function(){
+		_ctgList.querySelectorAll(".item").forEach(function(item){
+			item.addEventListener("click", function(){
+				_this.selectCategory(this);
+			});
+		});
+	}
+	/**
+	 * 카테고리 선택
+	 */
+	this.selectCategory = function(item){
+		_this.removeAllMark();
+		_this.addMark();
+	}
+	/**
+	 * 전체 카테고리의 표시 삭제 
+	 */
+	this.removeAllMark = function(){
+		_ctgList.querySelectorAll(".item > a").forEach(function(item){
+			item.classList.remove("active");
+		});
+	}
+	/**
+	 * 카테고리 표시 등록
+	 */
+	this.addMark = function(item){
+		item.querySelector("a").classList.add("active");
 	}
 }
--- src/main/webapp/js/mainpage.js
+++ src/main/webapp/js/mainpage.js
@@ -8,7 +8,8 @@ onload = function(){
 	_me.onload();
 }
 
-var _category = new Category(); // 카테고리
+var _ctgList = new CategoryList(); // 카테고리 리스트
+var _prdList = new ProductList();
 /**
  * 현재 페이지 객체
  */
@@ -17,14 +18,14 @@ var _me = {
 	 *  현재 페이지 onload 함수
 	 */
 	onload: function(){
-		console.log("mainPage onload!!!!!");
 		_me.initPage();
 	}
 	/**
 	 *	현재 페이지 초기화 함수
 	 */
 	,initPage: function(){
-		_category.onload();
+		_prdList.onload(document.querySelector("#prdList"));
+		_ctgList.onload(document.querySelector("#ctgList"),prdList);
 	}
 }
 // 1.GNB 영역
@@ -33,3 +34,59 @@ var _me = {
 // 4.총 개수표시 영역
 // 5.상품 리스트
 // 6.더보기 영역
+function ProductList(){
+	var _this = null;
+	var _prdList = null;
+	
+	this.onload = function(element){
+		_this = this;
+		_prdList = element;
+		_this.getData();
+	}
+	/**
+	 * 데이터 조회
+	 */
+	this.getData = function(start, categoryId){
+		// apiURL 설정
+		var url = "api/products";
+		
+		ajax(url, function() {
+			var jsonObj = JSON.parse(this.responseText);
+			var products = jsonObj.items;
+			
+			_this.initPrdList();
+			
+			for(var i=0;i<products.length;i++){
+				var item = products[i];
+				var div = "";
+				if(i%2==0)
+					div = "left";
+				else
+					div = "right";
+				_this.addProduct(item, div);
+			}
+			
+		}, false);
+	}
+	/**
+	 * 상품 리스트 초기화
+	 */
+	this.initPrdList = function(){
+		_prdList.querySelector("#left").innerHTML = "";
+		_prdList.querySelector("#right").innerHTML = "";
+	}
+	/**
+	 * 상품 추가
+	 */
+	this.addProduct = function(data, div){
+		var prdTemplate = document.querySelector("#itemList").innerHTML;
+		prdTemplate = prdTemplate.replaceAll("${id}",data.displayInfoId);
+		prdTemplate = prdTemplate.replaceAll("${placeName}",data.placeName);
+		prdTemplate = prdTemplate.replaceAll("${content}",data.productContent);
+		prdTemplate = prdTemplate.replaceAll("${description}",data.productDescription);
+		if(div==="left")
+			_prdList.querySelector("#left").insertAdjacentHTML("beforeend", prdTemplate);
+		else if(div==="right")
+			_prdList.querySelector("#right").insertAdjacentHTML("beforeend", prdTemplate);
+	}
+}
