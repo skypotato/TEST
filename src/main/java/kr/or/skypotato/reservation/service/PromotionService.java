cf5fc5965a668c222aa3fb3b6a9f44c188e48896
SkyPotato <hunsooski@naver.com>
2019-01-01 13:44:03 +0900

    201 Task-Url:

--- src/main/java/kr/or/skypotato/reservation/service/PromotionService.java
+++ src/main/java/kr/or/skypotato/reservation/service/PromotionService.java
@@ -2,7 +2,6 @@ package kr.or.skypotato.reservation.service;
 
 import java.util.List;
 
-import kr.or.skypotato.reservation.dto.Product;
 import kr.or.skypotato.reservation.dto.Promotion;
 
 public interface PromotionService {
--- src/main/java/kr/or/skypotato/reservation/service/impl/PromotionServiceImpl.java
+++ src/main/java/kr/or/skypotato/reservation/service/impl/PromotionServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.or.skypotato.reservation.dao.PromotionDao;
-import kr.or.skypotato.reservation.dto.Product;
 import kr.or.skypotato.reservation.dto.Promotion;
 import kr.or.skypotato.reservation.service.PromotionService;
 
